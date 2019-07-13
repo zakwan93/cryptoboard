296dcb99863a2714bac2d8c84017ca93047f4f24
zakwan93 <zakwan_bhaiyat@yahoo.com>
2019-07-13 17:48:09 -0400

    added .env file to ignore babel version error adding pre flight check to true

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
   return (
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          <code>Zakwan</code>
         </p>
         <a
           className="App-link"
