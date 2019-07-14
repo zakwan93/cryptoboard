4f7c38be252a9a00e80559b884baaeed68cdc6f3
zakwan93 <zakwan_bhaiyat@yahoo.com>
2019-07-13 23:10:09 -0400

    added global style.js and add style to coin grid

--- src/Settings/CoinGrid.js
+++ src/Settings/CoinGrid.js
@@ -1,10 +1,12 @@
 import React from "react";
 import styled from "styled-components";
 import { AppContext } from "../App/AppProvider";
+import { SelectableTile } from "../Shared/Tile";
 
 export const CoinGridStyle = styled.div`
   display: grid;
   grid-template-columns: repeat(5, 1fr);
+  grid-gap: 15px;
 `;
 
 export default function() {
@@ -13,7 +15,7 @@ export default function() {
       {({ coinList }) => (
         <CoinGridStyle>
           {Object.keys(coinList).map(coinKey => (
-            <div> {coinKey} </div>
+            <SelectableTile> {coinKey} </SelectableTile>
           ))}
         </CoinGridStyle>
       )}
--- src/Settings/ConfirmButton.js
+++ src/Settings/ConfirmButton.js
@@ -1,11 +1,17 @@
 import React from "react";
 import styled from "styled-components";
 import { AppContext } from "../App/AppProvider";
+import { fontSize1, greenBoxShadow, color3 } from "../Shared/Style";
 
 const ConfirmButtonStyled = styled.div`
   margin: 20px;
-  color: green;
+  color: ${color3};
+  ${fontSize1};
+  padding: 10px;
   cursor: pointer;
+  &:hover {
+    ${greenBoxShadow};
+  }
 `;
 export const CenterDiv = styled.div`
   display: grid;
