f30d3fb72e7f6fc2e44fec0560a182827999c59f
zakwan93 <zakwan_bhaiyat@yahoo.com>
2019-07-14 23:41:03 -0400

    responsive grid

--- src/Settings/CoinGrid.js
+++ src/Settings/CoinGrid.js
@@ -5,7 +5,8 @@ import CoinTile from "./CoinTile";
 
 export const CoinGridStyle = styled.div`
   display: grid;
-  grid-template-columns: repeat(5, 1fr);
+  grid-template-columns: repeat(auto-fill, minmax(130px, 1fr));
+  margin: 0 auto;
   grid-gap: 15px;
   margin: 40px 0;
 `;
