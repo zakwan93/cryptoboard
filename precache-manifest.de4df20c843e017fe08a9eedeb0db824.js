f9af75ce9114d0d83fcf0a9eb1390a07837e9489
zakwan93 <zakwan_bhaiyat@yahoo.com>
2019-07-16 22:55:46 -0400

    Updates

---
+++ precache-manifest.de4df20c843e017fe08a9eedeb0db824.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e7870733148b37bcaab3a06f081a3c95",
+    "url": "/cryptoboard/index.html"
+  },
+  {
+    "revision": "d2cecc441775564f4d1b",
+    "url": "/cryptoboard/static/css/main.f4e7ec4f.chunk.css"
+  },
+  {
+    "revision": "4035aefbd874a824beb3",
+    "url": "/cryptoboard/static/js/2.412f5393.chunk.js"
+  },
+  {
+    "revision": "d2cecc441775564f4d1b",
+    "url": "/cryptoboard/static/js/main.450ef146.chunk.js"
+  },
+  {
+    "revision": "529113d17c384049eafe",
+    "url": "/cryptoboard/static/js/runtime~main.117c916a.js"
+  }
+]);
